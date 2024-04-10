9edb6da7d94b9719c082df832aad824b0ca481a0
Jonhvmp <jonhpaz08@gmail.com>
2024-04-10 03:15:27 -0300

    Update funcoes001.py

---
+++ Conhecendo_a_linguagem/funcoes_de_entrada_e_saida/funcoes001.py
@@ -0,0 +1,11 @@
+# Metodos de entrada e saida
+#Nesse 'input' o cliente entra com um valor
+
+name = input("Informe o seu nome: ")
+age = input(("Agora informe a sua idadde: "))
+
+print(name, age)
+print(name, age, end="...\n")
+print(name, age, sep="#")
+
+print(5 // 2)
