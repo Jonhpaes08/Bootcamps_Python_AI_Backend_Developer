eadb915000fb310f7a4e1bef88af02a621037aff
Jonhvmp <jonhpaz08@gmail.com>
2024-04-10 02:06:19 -0300

    Update conversao001.py

---
+++ Conhecendo_a_linguagem/conversao_de_tipos/conversao001.py
@@ -0,0 +1,5 @@
+print(int(1.9898465)) #Estamos convertendo um número 'float' em 'int' se executar o seguint 'print' o resultado vai ser equivalente = 1 "não importa o número de casas, ele sempre vai retornar '1' "
+print(int("10")) #aqui convertemos uma string em número inteiro
+print(float("10.10")) #da mesma forma será nesse exemplo, ao executar ele vai tranformar em número 'float' = (10.1) que é a mesma coisa quê (10.10).
+
+
