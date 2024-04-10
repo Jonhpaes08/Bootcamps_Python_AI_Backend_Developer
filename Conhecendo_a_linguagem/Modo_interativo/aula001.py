73c9b982996cabc0efe776c7ba75e598a162f13e
Jonhvmp <jonhpaz08@gmail.com>
2024-04-09 23:43:29 -0300

    Variáveis e constantes

---
+++ Conhecendo_a_linguagem/Modo_interativo/aula001.py
@@ -0,0 +1,12 @@
+#Para entrar no modo interativo, entramos no termial e digitamos o seguinte código 'Python' e em seguida estaremos no modo interativo, podemos fazer testes rápidos. Para sair do modo interativo usamos o código 'exit()'
+
+
+#podemos usar esses códigos no modo interativo
+
+#dir()
+#dir(100)
+
+#podemos ver todas as funções de cada usando o 'help'
+ 
+#help(int)
+#help(float)
---
+++ Conhecendo_a_linguagem/Variaveis_e_constantes/variaveis001.py
@@ -0,0 +1,5 @@
+#Faremos um exemplo de código com variáveis
+
+age = 28 #aqui sabemos que o valor é 'Int'
+name = 'Jonh Alex'
+print(f'O cliente {name}, têm exatamente {age} anos de idade.') #usamos 'f' antes do conteudo e dentro do conteudo adicionamos as variáveis para que o sistema adicione automaticamente o no conteudo, ou seja, nesse caso acima demos as variáveis, entretanto elas podem variar de acordo com o projeto(sistema) o cliente vai adicionar um valor 'x' e esse valor irá aparecer no conteudo onde está localizado a variável.
